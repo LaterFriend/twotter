31788bb48433fdcbc43f4aa2ebf5d708e7f95424
Daniel Janda <87991817+LaterFriend@users.noreply.github.com>
2022-11-02 23:15:05 +0100

    Update UserProfile.vue

--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -1,12 +1,20 @@
 <template>
-    <div class="user-profile">
-        <div class="user-profile__user-panel">
-            <h1 class="user-profile__username">@{{user.username}}</h1>
-            <div class="user-profile__follower-count">
-                <strong>Followers: </strong> {{ followers }}
-            </div>
-        </div>
+  <div class="user-profile">
+    <div class="user-profile__user-panel">
+      <h1 class="user-profile__username">@{{ user.username }}</h1>
+      <div class="user-profile__admin-badge" v-if="user.isAdmin">
+        Admin
+      </div>
+      <div class="user-profile__follower-count">
+        <strong>Followers: </strong> {{ followers }}
+      </div>
     </div>
+    <div class="user-profile__twoots-wrapper">
+      <div class="user-profile__twoot" v-for="twoot in user.twoots" :key="twoot.id">
+        {{ twoot.content }}
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -21,7 +29,11 @@ export default {
         firstName: 'Daniel',
         lastName: 'Janda',
         email: 'danieljanda75@seznam.cz',
-        isAdmin: true
+        isAdmin: false,
+        twoots: [
+          {id: 1, content: 'Twotter is Amazing!'},
+          {id: 2, content: 'Hello World!'}
+        ]
       }
     }
   },
@@ -51,25 +63,33 @@ export default {
 </script>
 
 <style>
-
 .user-profile {
-    display: grid;
-    grid-template-columns: 1fr 3fr;
-    width: 100%;
-    padding: 50px 5%;
+  display: grid;
+  grid-template-columns: 1fr 3fr;
+  width: 100%;
+  padding: 50px 5%;
 }
 
 .user-profile__user-panel {
-    display: flex;
-    flex-direction: column;
-    margin-right: 50px;
-    padding: 20px;
-    background-color: white;
-    border-radius: 5px;
-    border: 1px solid #DFE3E8;
+  display: flex;
+  flex-direction: column;
+  margin-right: 50px;
+  padding: 20px;
+  background-color: white;
+  border-radius: 5px;
+  border: 1px solid #DFE3E8;
+}
+
+.user-profile__admin-badge {
+  background: rebeccapurple;
+  color: white;
+  border-radius: 5px;
+  margin-right: auto;
+  padding: 0px 10px;
+  font-weight: bold;
 }
 
 h1 {
-    margin: 0;
+  margin: 0;
 }
 </style>
